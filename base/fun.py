b37a9ffa90c6c513ab5d4a2384ada857107dc17e
xujiafeng <xujiafeng19940113>
2018-03-19 17:51:52 +0800

    commit

--- base/fun.py
+++ base/fun.py
@@ -6,3 +6,22 @@ def sum(n):
 
 
 print(sum(10))
+
+
+def sortList(list, isDesc=False):
+    for i in range(len(list)):
+        for j in range(1,len(list)-i):
+            flag=list[j] < list[j - 1]
+            if(isDesc):
+                flag=not flag
+            if (flag):
+                list[j], list[j - 1] = list[j - 1], list[j]
+    return list
+
+L=sortList([12,32,13,123,76,453,23,96])
+print(L)
+L=sortList([12,32,13,123,76,453,23,96],True)
+print(L)
+
+
+
